0b17bdba36dbc172e200e1aebec2377c7cbe8543
Luis Acuna <ldacuna83@gmail.com>
2021-04-09 23:29:01 -0500

    commit client base

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -30,7 +30,6 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
-            //
         }
     }
 }
